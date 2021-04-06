8202236df1b59b2250d5466501e7b6619c91060d
Mario Lang <mariolang1@gmx.de>
2021-04-06 20:29:20 +0200

    added events module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		pathMatch: 'full',
+		redirectTo: 'events',
+	},
+	{
+		path: 'events',
+		loadChildren: () => import('./events/events.module').then((m) => m.EventsModule),
+	},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
